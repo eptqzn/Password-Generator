399aa375217d4e6851abb5fd69b915d7528b3d8b
Enrich Quiazon <104322730+eptqzn@users.noreply.github.com>
2022-06-15 02:41:03 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+const passwordLength = 15;
+const generateButton = document.getElementById("generate-el");
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+
+
+function getRandomChar() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generatePassword() {
+    let randomPassword = " "
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomChar()
+    }
+    return randomPassword
+}
+
+generateButton.addEventListener("click", function() {
+   passwordOne.textContent = generatePassword()
+   passwordTwo.textContent = generatePassword()
+  }
+)
